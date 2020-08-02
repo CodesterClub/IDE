169c58b5c4d58bb82d3e2f64f55159d8b5844564
CodesterClub <69087761+CodesterClub@users.noreply.github.com>
2020-08-02 16:16:13 +0530

    Adding the source file

---
+++ IDE.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+code="" # The variable where the code to be compiled will be stored
+while :
+do
+read code
+	if [ X"$code" = X"stop" ]
+	then
+		break
+	else
+		echo "$code" >> IDE.java 
+	fi
+done
+#NOTE: The >> instead of > will not overwrite the file but will 
+# append the input to the file as we press enter to go to the next line
+
+# The following line will compile and exectute the IDE.java file
+javac IDE.java
+rm IDE.java
+java IDE
+rm IDE.class
+
+
